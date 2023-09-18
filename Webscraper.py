e885fd116ea7db0586ab64cb6d6f802f4d622150
adrian-sarnecki <adrian.sarnecki95@gmail.com>
2023-09-18 23:19:26 +0200

    18.09.2023 commit 03

--- Webscraper.py
+++ Webscraper.py
@@ -1,20 +1,25 @@
 from wdSettings import *
-from datetime import date
+import datetime
 
-#date input
-choosenDate = input("Enter date in format: DD/MM/YYYY: ")
-
-
-
-websiteURL = "https://www.whoscored.com/LiveScores"
-driver.get(websiteURL)
-
-# window close
-acceptWindow = driver.find_element(by="xpath", value=("//*[text()='AGREE']"));
-acceptWindow.click()
-
-# date choosing window according to input value
-dateWindow = driver.find_element(by="id", value="toggleCalendar")
-dateWindow.click()
+# date input - return as INT numbers
+enteredDate = input("Enter date in format: DD-MM-YYYY: ")
 
 
+year, month, day = map(int, enteredDate.split('-'))
+choosenDate = datetime.datetime(day, month, year)
+matchDay = choosenDate.day
+matchMonth = choosenDate.month
+matchYear = choosenDate.year
+
+
+
+# websiteURL = "https://www.whoscored.com/LiveScores"
+# driver.get(websiteURL)
+#
+# # window close
+# acceptWindow = driver.find_element(by="xpath", value=("//*[text()='AGREE']"));
+# acceptWindow.click()
+#
+# # date choosing window according to input value
+# dateWindow = driver.find_element(by="id", value="toggleCalendar")
+# dateWindow.click()
--- wdSettings.py
+++ wdSettings.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-wd_path = "A:\Programowanie\Projekty\Python\Python-webscraper\chromedriver.exe"  # path to chromedriver, remember to choose your own
+wd_path = "A:\Programowanie\Projekty\Python\Selenium-webscraper\chromedriver.exe"  # path to chromedriver, remember to choose your own
 
 options = Options()
 options.add_experimental_option("detach", True)
